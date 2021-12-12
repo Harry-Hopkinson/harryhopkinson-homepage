8851699796559569927773631ee39e897102f1f1
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-12 09:04:27 +0000

    Started a black and white colour theme

--- components/navbar.js
+++ components/navbar.js
@@ -78,17 +78,34 @@ const Navbar = props => {
         <Box flex={1} align="right">
             <Box ml={2} display={{base: "inline-block", md: "none"}}>
                 <Menu>
-                    <MenuButton
-                        as={IconButton}
-                        icon={<HamburgerIcon />}
-                        variant="outline"
-                        aria-label="Options"
-                    />
-                </Menu>
-            </Box> 
-        </Box>    
-            </Container>
+                <MenuButton
+                    as={IconButton}
+                    icon={<HamburgerIcon />}
+                    variant="outline"
+                    aria-label="Options"
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works" passHref>
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/posts" passHref>
+                  <MenuItem as={Link}>Posts</MenuItem>
+                </NextLink>
+                <MenuItem
+                  as={Link}
+                  href="https://github.com/Harry-Hopkinson/harryhopkinson-homepage"
+                >
+                  View Source
+                  </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
         </Box>
+      </Container>
+    </Box>
     )
 }
 
