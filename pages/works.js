1502745a97ad987aa5365e61beebced5e5667672
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-14 21:39:44 +0000

    Trying to fix bug

--- pages/works.js
+++ pages/works.js
@@ -14,6 +14,29 @@ const Works = () => {
             <Heading as="h3" fontSize={20} mb={4}>
                 Works
             </Heading>
+
+            <SimpleGrid columns={[1, 2, 3]} gap={6}>
+                <Section>
+                    <WorkGridItem id="snow-ball-brawl" title="Snow Ball Brawl" thumbnail={snowballbrawl}>
+                        SnowBallBrawl - A Two Player Game in which you fight with Snowballs.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="kingfisher-code" title="Kingfisher Code" thumbnail={kingfishercode}>
+                        A Beginner Level Programming Language Built in Python.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="booflix" title="BookFlix" thumbnail={bookflix}>
+                        A Place to Manage, Explore and Review Books from Your Browser.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="notebook-todo" title="NoteBook Todo" thumbnail={noteBookTODO}>
+                        A Notebook to Store Ideas and Code Snippets. Can Import Images and Graphs to Help Consolodate Knowledge.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
         </Container>
     )
 }
