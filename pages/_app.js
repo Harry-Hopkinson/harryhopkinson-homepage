08ff77f5508b713aa0b5b735e98133e260f3899e
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-14 20:31:37 +0000

    Fixed bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Layout from "../components/layouts/main";
-import Fonts from "../components/fonts";
-import theme from "../lib/theme"
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+import Fonts from '../components/fonts'
+import theme from '../lib/theme'
+import { AnimatePresence } from 'framer-motion'
 
-const Website = ({ Component, pageProps, router }) => {
-    return (
-        <ChakraProvider theme={theme}>
-            <Fonts />
-            <Layout router={router}>
-                <Component {...pageProps} key = {router.route}/>
-            </Layout>
-        </ChakraProvider>
-    )
+function Website({ Component, pageProps, router }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Layout router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default Website
