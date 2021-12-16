4b0fb8cdedfeeac6f1aaeb0681598b43177411f6
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-16 16:06:36 +0000

    Fixed errors

--- pages/works.js
+++ pages/works.js
@@ -5,7 +5,7 @@ import Section from '../components/section';
 import { snowballbrawl } from '../public/images/works/snowballbrawl.PNG';
 import { bookflix } from "../public/images/works/bookflix.png";
 import { kingfishercode } from "../public/images/works/kingfishercode.PNG";
-import { noteBookTODO } from "../public/images/works/notebookTODO.PNG";
+import { noteBookTODO } from "../public/images/works/notebookTODO.png";
 
 
 const Works = () => {
@@ -16,26 +16,7 @@ const Works = () => {
             </Heading>
 
             <SimpleGrid columns={[1, 2, 3]} gap={6}>
-                <Section>
-                    <WorkGridItem id="snow-ball-brawl" title="Snow Ball Brawl" thumbnail={snowballbrawl}>
-                        SnowBallBrawl - A Two Player Game in which you fight with Snowballs.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="kingfisher-code" title="Kingfisher Code" thumbnail={kingfishercode}>
-                        A Beginner Level Programming Language Built in Python.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="booflix" title="BookFlix" thumbnail={bookflix}>
-                        A Place to Manage, Explore and Review Books from Your Browser.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="notebook-todo" title="NoteBook Todo" thumbnail={noteBookTODO}>
-                        A Notebook to Store Ideas and Code Snippets. Can Import Images and Graphs to Help Consolodate Knowledge.
-                    </WorkGridItem>
-                </Section>
+                
             </SimpleGrid>
         </Container>
     )
