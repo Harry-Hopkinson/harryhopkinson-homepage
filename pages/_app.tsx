30399fc99dec2ace5e45eed3a0aeb58f6fa3da1a
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-22 12:00:11 +0000

    Fixed file extension error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion';
-import Chakra from "../components/chakra.tsx";
+import Chakra from "../components/chakra.ts";
 
 function Website({ Component, pageProps, router }) {
   return (
