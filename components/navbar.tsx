38a44a2d684bbd6713ab2fa276769ad6d8138a08
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-22 12:26:58 +0000

    Fixed typescript error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -71,7 +71,7 @@ const Navbar = props => {
           flexGrow={1}
           mt={{ base: 4, md: 0 }}
         >
-          <LinkItem href="/works" path={path}>
+          <LinkItem href="/works" path={path} _target={null}>
             Works
           </LinkItem>
           <LinkItem
