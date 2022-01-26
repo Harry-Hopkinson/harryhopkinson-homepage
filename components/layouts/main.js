1965b23e951c7adf622aa353431ecf2bcaa77ede
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-26 11:00:18 +0000

    Removed voxel dog due to copyright

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -3,12 +3,6 @@ import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
-
-const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-  ssr: false,
-  loading: () => <VoxelDogLoader />
-})
 
 const Main = ({ children, router }) => {
   return (
@@ -25,7 +19,6 @@ const Main = ({ children, router }) => {
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        <LazyVoxelDog />
         {children}
         <Footer />
       </Container>
